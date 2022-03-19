89382230d4e95488dce834ee148758c5c2a3225b
RosiNicolJiron <rosi.jiron@unah.hn>
2022-03-19 15:23:49 -0600

    Mi Primer commit Pedidos Proveedor

---
+++ config/conexion.php
@@ -0,0 +1,20 @@
+<?php
+   Class Conectar {
+       protected $dbh;
+
+
+       protected function Conexion(){
+           try {
+               $conectar = $this->dbh = new PDO("mysql:host=52.152.236.67;dbname=g6_19","G6_19","D7OEynOQ");
+               return $conectar;
+            }catch(Exception $e){
+                print"¡Error BD!:" . $e->getMessage() . "<br/>";
+                die();
+            }
+        }
+        public function set_names(){
+            return $this->dbh->query("SET NAMES 'utf8'");
+        }
+    }
+
+?>
